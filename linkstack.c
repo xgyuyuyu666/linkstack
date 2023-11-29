3e31e5030fa3e438f9faa5b4b903cccb977bd4d9
鱼鱼鱼 <945603334@qq.com>
2023-11-29 11:25:12 +0800

    链式栈

---
+++ linkstack.c
@@ -0,0 +1,8 @@
+//
+// Created by 鱼鱼鱼 on 2023/11/29.
+//
+
+
+#include <stdio.h>
+#include"linkstack.h"
+
---
+++ linkstack.h
@@ -0,0 +1,8 @@
+//
+// Created by 鱼鱼鱼 on 2023/11/29.
+//
+
+#ifndef LINKSTACK_LINKSTACK_H
+#define LINKSTACK_LINKSTACK_H
+
+#endif //LINKSTACK_LINKSTACK_H
